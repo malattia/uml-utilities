8768495e4b34d4b33ff2e54f08bc5d79f7b3f0f2
Jeff Dike <jdike@addtoit.com>
2001-09-08 01:11:15 +0000

    Added 'quit' and 'switch' to the mconsole. Also added some local help.

--- mconsole/uml_mconsole.c
+++ mconsole/uml_mconsole.c
@@ -2,11 +2,114 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <ctype.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <sys/uio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+
+static struct sockaddr_un sun;
+
+static void do_default(int fd, char *command)
+{
+  struct iovec iov;
+  struct msghdr msg;
+  struct cmsghdr *cmsg;
+  struct ucred *credptr;
+  char anc[CMSG_SPACE(sizeof(*credptr))];
+  char buf[256];
+  int n;
+
+  iov.iov_base = command;
+  iov.iov_len = strlen(command);
+
+  msg.msg_control = anc;
+  msg.msg_controllen = sizeof(anc);
+
+  cmsg = CMSG_FIRSTHDR(&msg);
+  cmsg->cmsg_level = SOL_SOCKET;
+  cmsg->cmsg_type = SCM_CREDENTIALS;
+  cmsg->cmsg_len = CMSG_LEN(sizeof(*credptr));
+  credptr = (struct ucred *) CMSG_DATA(cmsg);
+  credptr->pid = getpid();
+  credptr->uid = getuid();
+  credptr->gid = getgid();
+
+  msg.msg_name = &sun;
+  msg.msg_namelen = sizeof(sun);
+  msg.msg_iov = &iov;
+  msg.msg_iovlen = 1;
+  msg.msg_controllen = cmsg->cmsg_len;
+  msg.msg_flags = 0;
+
+  if(sendmsg(fd, &msg, 0) < 0){
+    perror("sendmsg");
+    exit(1);
+  }
+
+  iov.iov_base = buf;
+  iov.iov_len = sizeof(buf);
+
+  msg.msg_name = NULL;
+  msg.msg_namelen = 0;
+  msg.msg_iov = &iov;
+  msg.msg_iovlen = 1;
+  msg.msg_control = NULL;
+  msg.msg_controllen = 0;
+  msg.msg_flags = 0;
+
+  n = recvmsg(fd, &msg, 0);
+  if(n < 0){
+    perror("recvmsg");
+    exit(1);
+  }
+  buf[n] = '\0';
+  printf("%s\n", buf);
+}
+
+static void do_help(int fd, char *command)
+{
+  do_default(fd, command);
+  printf("Additional local mconsole commands:\n");
+  printf("    quit - Quit mconsole\n");
+  printf("    switch <socket-name> - Switch control to the given machine\n");
+}
+
+static void do_switch(int fd, char *command)
+{
+  struct stat sbuf;
+  char *ptr;
+
+  ptr = &command[strlen("switch")];
+  while(isspace(*ptr)) ptr++;
+  if(stat(ptr, &sbuf) == -1){
+    fprintf(stderr, "Couldn't stat file: %s - ", ptr);
+    perror("");
+  }
+  else {
+    strcpy(sun.sun_path, ptr);
+    printf("Switched to '%s'\n", ptr);
+  }
+}
+
+static void do_quit(int fd, char *command)
+{
+  exit(0);
+}
+
+struct cmd {
+  char *command;
+  void (*proc)(int, char *);
+};
+
+static struct cmd cmds[] = {
+  { "quit", do_quit },
+  { "help", do_help },
+  { "switch", do_switch },
+  { NULL, do_default }
+};
 
 static void Usage(void)
 {
@@ -16,7 +119,7 @@ static void Usage(void)
 
 int main(int argc, char **argv)
 {
-  struct sockaddr_un sun, here;
+  struct sockaddr_un here;
   char *sock;
   int fd;
 
@@ -38,63 +141,24 @@ int main(int argc, char **argv)
   }
 
   while(1){
-    struct iovec iov;
-    struct msghdr msg;
-    struct cmsghdr *cmsg;
-    struct ucred *credptr;
-    char anc[CMSG_SPACE(sizeof(*credptr))];
-    char buf[256], *command;
-    int n;
+    char *command, *ptr;
+    int i;
 
     command = readline("(mconsole) ");
     if(command == NULL) break;
 
-    iov.iov_base = command;
-    iov.iov_len = strlen(command);
+    /* Trim trailing spaces left by readline's filename completion */
+    ptr = &command[strlen(command) - 1];
+    while(isspace(*ptr)) *ptr-- = '\0';
 
-    msg.msg_control = &anc;
-    msg.msg_controllen = sizeof(buf);
-
-    cmsg = CMSG_FIRSTHDR(&msg);
-    cmsg->cmsg_level = SOL_SOCKET;
-    cmsg->cmsg_type = SCM_CREDENTIALS;
-    cmsg->cmsg_len = CMSG_LEN(sizeof(*credptr));
-    credptr = (struct ucred *) CMSG_DATA(cmsg);
-    credptr->pid = getpid();
-    credptr->uid = getuid();
-    credptr->gid = getgid();
-
-    msg.msg_name = &sun;
-    msg.msg_namelen = sizeof(sun);
-    msg.msg_iov = &iov;
-    msg.msg_iovlen = 1;
-    msg.msg_controllen = cmsg->cmsg_len;
-    msg.msg_flags = 0;
-
-    if(sendmsg(fd, &msg, 0) < 0){
-      perror("sendmsg");
-      exit(1);
+    for(i = 0; i < sizeof(cmds)/sizeof(cmds[0]); i++){
+      if((cmds[i].command == NULL) || 
+	 !strncmp(cmds[i].command, command, strlen(cmds[i].command))){
+	(*cmds[i].proc)(fd, command);
+	break;
+      }
     }
     free(command);
-
-    iov.iov_base = buf;
-    iov.iov_len = sizeof(buf);
-
-    msg.msg_name = NULL;
-    msg.msg_namelen = 0;
-    msg.msg_iov = &iov;
-    msg.msg_iovlen = 1;
-    msg.msg_control = NULL;
-    msg.msg_controllen = 0;
-    msg.msg_flags = 0;
-
-    n = recvmsg(fd, &msg, 0);
-    if(n < 0){
-      perror("recvmsg");
-      exit(1);
-    }
-    buf[n] = '\0';
-    printf("%s\n", buf);
   }
   printf("\n");
   return(0);
