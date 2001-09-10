3928dbbe259080693128152fa4a0a63b59dbf8a0
Jeff Dike <jdike@addtoit.com>
2001-09-10 02:16:58 +0000

    mconsole no longer exits on communication errors with UML. The prompt is now formed from the UML machine name. 'name: command' syntax is now accepted and sends 'command' to the UML named 'name'. A machine name may be passed on the command line and to 'switch'. Command history now works.

--- mconsole/uml_mconsole.c
+++ mconsole/uml_mconsole.c
@@ -4,23 +4,65 @@
 #include <errno.h>
 #include <ctype.h>
 #include <sys/stat.h>
+#include <sys/param.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <sys/uio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
+static char uml_name[11];
 static struct sockaddr_un sun;
 
-static void do_default(int fd, char *command)
+static int do_switch(char *name)
+{
+  struct stat buf;
+
+  if(stat(name, &buf) == -1){
+    fprintf(stderr, "Couldn't stat file: %s - ", name);
+    perror("");
+    return(-1);
+  }
+  sun.sun_family = AF_UNIX;
+  strcpy(sun.sun_path, name);
+  if(sscanf(name, "/tmp/uml/%10[^/]/mconsole", uml_name) < 1){
+    printf("Couldn't determine UML name from '%s'\n", name);
+    strcpy(uml_name, "[Unknown]");
+  }
+  return(0);
+}
+
+static int switch_common(char *name)
+{
+  char file[MAXPATHLEN];
+  int try_file = 1;
+
+  sprintf(file, "/tmp/uml/%s/mconsole", name);
+  if(strncmp(name, "/tmp/uml/", strlen("/tmp/uml/"))){
+    if(!do_switch(file)) return(0);
+    try_file = 0;
+  }
+  if(!do_switch(name)) return(0);
+  if(!try_file) return(-1);
+  return(do_switch(file));
+}
+
+static void default_cmd(int fd, char *command)
 {
   struct iovec iov;
   struct msghdr msg;
   struct cmsghdr *cmsg;
   struct ucred *credptr;
   char anc[CMSG_SPACE(sizeof(*credptr))];
-  char buf[256];
+  char buf[256], name[128];
   int n;
+
+  if(sscanf(command, "%128[^: \f\n\r\t\v]:", name) == 1){
+    if(switch_common(name)) return;
+    command = strchr(command, ':');
+    *command++ = '\0';
+    while(isspace(*command)) command++;
+  }
 
   iov.iov_base = command;
   iov.iov_len = strlen(command);
@@ -45,8 +87,9 @@ static void do_default(int fd, char *command)
   msg.msg_flags = 0;
 
   if(sendmsg(fd, &msg, 0) < 0){
-    perror("sendmsg");
-    exit(1);
+    fprintf(stderr, "Sending command to '%s' : ", sun.sun_path);
+    perror("");
+    return;
   }
 
   iov.iov_base = buf;
@@ -63,38 +106,31 @@ static void do_default(int fd, char *command)
   n = recvmsg(fd, &msg, 0);
   if(n < 0){
     perror("recvmsg");
-    exit(1);
+    return;
   }
   buf[n] = '\0';
   printf("%s\n", buf);
 }
 
-static void do_help(int fd, char *command)
+static void help_cmd(int fd, char *command)
 {
-  do_default(fd, command);
+  default_cmd(fd, command);
   printf("Additional local mconsole commands:\n");
   printf("    quit - Quit mconsole\n");
   printf("    switch <socket-name> - Switch control to the given machine\n");
 }
 
-static void do_switch(int fd, char *command)
+static void switch_cmd(int fd, char *command)
 {
-  struct stat sbuf;
   char *ptr;
 
   ptr = &command[strlen("switch")];
   while(isspace(*ptr)) ptr++;
-  if(stat(ptr, &sbuf) == -1){
-    fprintf(stderr, "Couldn't stat file: %s - ", ptr);
-    perror("");
-  }
-  else {
-    strcpy(sun.sun_path, ptr);
-    printf("Switched to '%s'\n", ptr);
-  }
+  if(switch_common(ptr)) return;
+  printf("Switched to '%s'\n", ptr);
 }
 
-static void do_quit(int fd, char *command)
+static void quit_cmd(int fd, char *command)
 {
   exit(0);
 }
@@ -105,10 +141,10 @@ struct cmd {
 };
 
 static struct cmd cmds[] = {
-  { "quit", do_quit },
-  { "help", do_help },
-  { "switch", do_switch },
-  { NULL, do_default }
+  { "quit", quit_cmd },
+  { "help", help_cmd },
+  { "switch", switch_cmd },
+  { NULL, default_cmd }
 };
 
 static void Usage(void)
@@ -124,9 +160,9 @@ int main(int argc, char **argv)
   int fd;
 
   if(argc < 2) Usage();
+  strcpy(uml_name, "[None]");
   sock = argv[1];
-  sun.sun_family = AF_UNIX;
-  strcpy(sun.sun_path, sock);
+  switch_common(sock);
 
   if((fd = socket(AF_UNIX, SOCK_DGRAM, 0)) < 0){
     perror("socket");
@@ -134,6 +170,7 @@ int main(int argc, char **argv)
   }
   here.sun_family = AF_UNIX;
   memset(here.sun_path, 0, sizeof(here.sun_path));
+
   sprintf(&here.sun_path[1], "%5d", getpid());
   if(bind(fd, (struct sockaddr *) &here, sizeof(here)) < 0){
     perror("bind");
@@ -141,11 +178,14 @@ int main(int argc, char **argv)
   }
 
   while(1){
-    char *command, *ptr;
+    char *command, *ptr, prompt[1 + sizeof(uml_name) + 2 + 1];
     int i;
 
-    command = readline("(mconsole) ");
+    sprintf(prompt, "(%s) ", uml_name);
+    command = readline(prompt);
     if(command == NULL) break;
+
+    if(*command) add_history(command);
 
     /* Trim trailing spaces left by readline's filename completion */
     ptr = &command[strlen(command) - 1];
