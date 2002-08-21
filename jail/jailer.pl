0f67afb3dfdf36cfca36baa3c0c5aa7590a771cc
Jeff Dike <jdike@addtoit.com>
2002-08-21 20:37:13 +0000

    This is more careful about its path. Added tty logging support. It will preface commands with sudo if necessary.

--- jail/jailer.pl
+++ jail/jailer.pl
@@ -3,19 +3,29 @@
 use strict;
 use English;
 
+# Make sure things like ifconfig and route are accessible
+$ENV{PATH} .= ":/sbin:/usr/sbin";
+
 sub Usage {
     print "Usage : jailer.pl uml-binary root-filesystem uid " .
-	"[ -net host-ip uml-ip] uml arguments ...\n";
+	"[ -net host-ip uml-ip] [ -tty-log ] uml arguments ...\n";
     exit 1;
 }
 
 my ($uml, $rootfs, $uid, @uml_args) = @ARGV;
 my ($tap, $host_ip, $uml_ip);
+my @net_cmds = ();
+my $tty_log = 0;
 
 while(1){
     if($uml_args[0] eq "-net"){
 	(undef, $host_ip, $uml_ip, @uml_args) = @uml_args;
 	(!defined($uml_ip) or !defined($host_ip)) and Usage();
+	@net_cmds = ( "tunctl", "ifconfig", "route" );
+    }
+    elsif($uml_args[0] eq "-tty-log"){
+	shift @uml_args;
+	$tty_log = 1;
     }
     else {
 	last;
@@ -24,8 +34,26 @@ while(1){
 
 (!defined($uml) || !defined($rootfs)) and Usage();
 
+my @cmds = ("mkdir", "tar", "cp", "basename", "rm", @net_cmds );
+
 my $sudo = "";
-$UID != 0 and $sudo = "sudo";
+if($UID != 0){
+    $sudo = "sudo";
+    push @cmds, $sudo;
+}
+
+my @dont_have = ();
+
+foreach my $cmd (@cmds){
+    `which $cmd 2>&1 > /dev/null`;
+    $? != 0 and push @dont_have, $cmd;
+}
+
+if(@dont_have){
+    print "Can't find the following utilities: " . 
+	join(" ", @dont_have) . "\n";
+    exit 1;
+}
 
 my $n = 0;
 
@@ -49,10 +77,17 @@ if(defined($host_ip)){
 
 	$out = `$sudo route add -host $uml_ip dev $tap 2>&1`;
 	$? ne 0 and die "Failed to set route to $uml_ip through $tap : '$out'";
+
+	$out = `$sudo echo 1 > /proc/sys/net/ipv4/ip_forward`;
+	$? ne 0 and die "Failed to enable IP forwarding : '$out'";
     }
     else {
 	die "Couldn't find tap device name in '$out'";
     }
+}
+
+if($tty_log == 1){
+    push @more_args, "3>tty_log_$cell", "tty_log_fd=3"
 }
 
 print "New inmate assigned to '$cell'\n";
@@ -64,6 +99,9 @@ if(defined($tap)){
 else {
     print "	No network configured\n";
 }
+if($tty_log == 1){
+    print "	TTY logging to tty_log_$cell\n";
+}
 print "	Extra arguments : '" . join(" ", @uml_args) . "'\n";
 print "\n";
 
@@ -73,6 +111,12 @@ $? ne 0 and die "Couldn't mkdir $cell : '$out'";
 my $cell_tar = "cell.tar";
 $out = `cd $cell ; $sudo tar xpf ../$cell_tar 2>&1`;
 $? ne 0 and die "Couldn't populate $cell : '$out'";
+
+$out = `$sudo chown $uid $cell/tmp 2>&1`;
+$? ne 0 and die "chown of /tmp failed : '$out'";
+
+$out = `$sudo chmod 777 $cell/tmp 2>&1`;
+$? ne 0 and die "chmod of /tmp failed : '$out'";
 
 print "Copying '$uml' and '$rootfs' to '$cell'...";
 $out = `cp $uml $rootfs $cell 2>&1`;
@@ -85,8 +129,11 @@ chomp $uml;
 $rootfs = `basename $rootfs`;
 chomp $rootfs;
 
-my @args = ("./jail_uml", $cell, $uid, "/$uml", "ubd0=/$rootfs", @uml_args,
-	    @more_args );
+$out = `$sudo chmod 666 $cell/$rootfs 2>&1`;
+$? ne 0 and die "chmod of $cell/$rootfs failed : '$out'";
+
+my @args = ("./jail_uml", $cell, $uid, "sh", "-c", "/$uml", "ubd0=/$rootfs", 
+	    @uml_args, @more_args );
 $sudo ne "" and unshift @args, $sudo;
 
 system @args;
@@ -102,4 +149,4 @@ if(defined($tap)){
     $? ne 0 and die "Failed to make $tap non-persistent : '$out'";
 }
 
-return 0;
+exit 0;
