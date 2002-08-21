e6200eef2a9f2f55e2d3ea62dd3aaa195cf6cd90
Jeff Dike <jdike@addtoit.com>
2002-08-21 20:36:44 +0000

    Restructured the code. Added remove support.

--- honeypot/hppfs.pm
+++ honeypot/hppfs.pm
@@ -32,38 +32,71 @@ sub add {
     }
 }
 
+sub prepare {
+    my $me = shift;
+    my $dir = shift;
+    my $file = shift;
+
+    my $full = $me->{base} . "/proc/$dir";
+    if(! -d $full){
+	unlink $full;
+	my $out = `mkdir -p $full 2>&1`;
+	$? and die "mkdir '$full' failed : $out";
+    }
+
+    defined($file) and unlink "$full/$file";
+    return("$full/$file");
+}
+
+sub setup_sock {
+    my ($me, $file, undef, $mode) = @_;
+
+    my $full = $me->prepare($file, $mode);
+
+    my $sock = sockaddr_un($full);
+    !defined($sock) and die "sockaddr_un of '$sock' failed : $!";
+
+    !defined(socket(my $fh, AF_UNIX, SOCK_STREAM, 0)) and 
+	die "socket failed : $!";
+
+    !defined(bind($fh, $sock)) and die "bind failed : $!";
+    my $out = `chmod 777 $full 2>&1`;
+    $? ne 0 and die "'chmod 777 $full' failed : $!";
+
+    !defined(listen($fh, 5)) and die "listen failed : $!";
+
+    $me->{select}->add(\*$fh);
+    $me->{handles}->{fileno(\*$fh)} = $file;
+}
+
+sub setup_remove {
+    my ($me, $file) = @_;
+
+    my $full = $me->prepare($file);
+
+    my $out = `touch $full/remove 2>&1`;
+    $? != 0 and die "touch $full/remove failed : $out";
+}
+
 sub handler {
     my $me = shift;
-    my $s = IO::Select->new();
+    $me->{select} = IO::Select->new();
 
     foreach my $file (keys(%{$me->{files}})){
-	my (undef, $mode) = @{$me->{files}->{$file}};
-
-	my $full = $me->{base} . "/proc/$file";
-	if(! -d $full){
-	    unlink $full;
-	    my $out = `mkdir -p $full 2>&1`;
-	    $? and die "mkdir '$full' failed : $out";
+	my $handler = $me->{files}->{$file};
+	if(ref($handler) eq "ARRAY"){
+	    $me->setup_sock($file, @$handler);
 	}
-	$full .= "/$mode";
-	unlink $full;
-
-	my $sock = sockaddr_un($full);
-	!defined($sock) and die "sockaddr_un of '$sock' failed : $!";
-
-	!defined(socket(my $fh, AF_UNIX, SOCK_STREAM, 0)) and 
-	    die "socket failed : $!";
-
-	!defined(bind($fh, $sock)) and die "bind failed : $!";
-
-	!defined(listen($fh, 5)) and die "listen failed : $!";
-
-	$s->add(\*$fh);
-	$me->{handles}->{fileno(\*$fh)} = $file;
+	elsif($handler eq "remove"){
+	    $me->setup_remove($file);
+	}
+	else {
+	    die "Bad handler for '$file'";
+	}
     }
 
     while(1){
-	my @ready = $s->can_read();
+	my @ready = $me->{select}->can_read();
 	
 	foreach my $sock (@ready){
 	    my $file = $me->{handles}->{fileno($sock)};
