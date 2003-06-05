7723948c2b56dc1c1b22e5fad5bd90b71a758850
Jeff Dike <jdike@addtoit.com>
2003-06-05 19:35:29 +0000

    Removed code and declarations which are now in cow.c.  This is everything having to do with the COW header.

--- moo/uml_moo.c
+++ moo/uml_moo.c
@@ -16,56 +16,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/param.h>
-#include <netinet/in.h>
-
-#if __BYTE_ORDER == __BIG_ENDIAN
-# define ntohll(x) (x)
-# define htonll(x) (x)
-#elif __BYTE_ORDER == __LITTLE_ENDIAN
-# define ntohll(x)  bswap_64(x)
-# define htonll(x)  bswap_64(x)
-#else
-#error "__BYTE_ORDER not defined"
-#endif
-
-typedef long long u64;
-
-/* XXX All these fields should be word-length independent */
-
-struct cow_header_common {
-       unsigned long magic;
-       unsigned long version;
-};
-
-/* taken from ubd_user.c */
-
-#define PATH_LEN_V1 256
-
-struct cow_header_v1 {
-	struct cow_header_common common;
-	char backing_file[PATH_LEN_V1];
-	time_t mtime;
-	u64 size;
-	int sectorsize;
-};
-
-#define PATH_LEN_V2 MAXPATHLEN
-
-struct cow_header_v2 {
-	struct cow_header_common common;
-	char backing_file[PATH_LEN_V2];
-	time_t mtime;
-	u64 size;
-	int sectorsize;
-};
- 
-union cow_header {
-       struct cow_header_v1 v1;
-       struct cow_header_v2 v2;
-};
-
-#define COW_MAGIC 0x4f4f4f4d  /* mooo */
-#define COW_VERSION 1
+#include "cow.h"
 
 static inline int ubd_test_bit(int bit, unsigned long *data)
 {
@@ -87,29 +38,17 @@ static inline void ubd_set_bit(int bit, unsigned long *data)
 	data[n] |= (1 << off);
 }
 
-static void sizes(u64 size, int sectorsize, int bitmap_offset, 
-		  int *bitmap_len_out, int *data_offset_out)
-{
-	*bitmap_len_out = (size + sectorsize - 1) / (8 * sectorsize);
-
-        *data_offset_out = bitmap_offset + *bitmap_len_out;
-	*data_offset_out = (*data_offset_out + sectorsize - 1) / sectorsize;
-	*data_offset_out *= sectorsize;
-}
-
 int create_backing_file(char *in, char *out)
 {
 	int cow_fd, back_fd, in_fd, out_fd;
 	struct stat buf;
 	unsigned long *bitmap;
-	int bitmap_len;
+	unsigned long bitmap_len;
 	int sectors;
 	void *sector, *zeros;
-	u64 size, offset, i; /* i is u64 to prevent 32 bit overflow */
-        union cow_header header;
-	struct cow_header_common common;
+	__u64 size, offset, i; /* i is u64 to prevent 32 bit overflow */
 	int data_offset;
-        unsigned long magic, version;
+        __u32 magic;
         char *backing_file;
         time_t mtime;
         int sectorsize, bitmap_offset, perms, n;
@@ -119,52 +58,11 @@ int create_backing_file(char *in, char *out)
 		exit(1);
 	}
 
-	if(read(cow_fd, &common, sizeof(common)) != sizeof(common)){
-		perror("COW common header read");
+	if(read_cow_header(cow_fd, &magic, &backing_file, &mtime, &size,
+			   &sectorsize, &bitmap_offset)){
+		fprintf(stderr, "Reading COW header failed\n");
 		exit(1);
 	}
-	if(lseek(cow_fd, 0, SEEK_SET) != 0){
-		perror("seeking back to COW file start");
-		exit(1);
-	}
-
-        magic = common.magic;
-        if(magic == COW_MAGIC) version = common.version;
-        else if(magic == ntohl(COW_MAGIC)) version = ntohl(common.version);
-	else {
-                fprintf(stderr,"magic number (0x%x) doesn't match COW_MAGIC "
-			"(0x%x)\n", (unsigned int) magic, COW_MAGIC);
-                exit(1);
-        }
-
-	if(version == 1){
-		if(read(cow_fd, &header.v1, sizeof(header.v1)) != 
-		   sizeof(header.v1)){
-			perror("Reading full V1 COW header");
-			exit(1);
-		}
-		backing_file = header.v1.backing_file;
-		size = header.v1.size;
-		mtime = header.v1.mtime;
-		sectorsize = header.v1.sectorsize;
-		bitmap_offset = sizeof(header.v1);
-	}
-	else if(version == 2){
-		if(read(cow_fd, &header.v2, sizeof(header.v2)) != 
-		   sizeof(header.v2)){
-			perror("Reading full V2 COW header");
-			exit(1);
-		}
-		backing_file = header.v2.backing_file;
-		size = ntohll(header.v2.size);
-		mtime = ntohl(header.v2.mtime);
-		sectorsize = ntohl(header.v2.sectorsize);
-		bitmap_offset = sizeof(header.v2);
-	} 
-	else {
-		fprintf(stderr,"Unknown version %ld\n", version);
-                exit(1);
-        }
 
 	if(stat(backing_file, &buf) < 0) {
 		perror("Stating backing file");
@@ -197,7 +95,7 @@ int create_backing_file(char *in, char *out)
 	}
 	else out_fd = back_fd;
 
-	sizes(size, sectorsize, bitmap_offset, &bitmap_len, &data_offset);
+	cow_sizes(size, sectorsize, bitmap_offset, &bitmap_len, &data_offset);
 		
 	bitmap = (unsigned long *) malloc(bitmap_len);
 	if(bitmap == NULL) {
