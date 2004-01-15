4f1c5c5c6d86f49f403b082c78654b0e5bb59774
Jeff Dike <jdike@addtoit.com>
2004-01-15 22:08:23 +0000

    Added bridging support and cleaned up the code.

--- jail/jailer.pl
+++ jail/jailer.pl
@@ -7,9 +7,31 @@ use English;
 $ENV{PATH} .= ":/sbin:/usr/sbin";
 
 sub Usage {
-    print "Usage : jailer.pl uml-binary root-filesystem uid " .
-	"[ -v ] [ -bridge uml-ip ] [ -net host-ip uml-ip] [ -tty-log ] " . 
-	"uml arguments ...\n";
+    print "Usage : jailer.pl uml-binary root-filesystem uid [ -n ] " .
+	"[ -v ]\n";
+    print "\t[ -net 0.0.0.0 uml-ip -bridge device,device,... ]\n";
+    print "\t[ -net host-ip uml-ip] [ -tty-log ] [ more uml arguments ... ]\n";
+    print "Required arguments:\n";
+    print "\tuml-binary is the path of the UML 'linux' executable\n\n";
+    print "\troot-filesystem is the path of the filesystem that it will " .
+	"boot on.\n";
+    print "\tThis will be copied into the jail, and the copy will be "
+	. "booted\n\n";
+    print "\tuid is the user id that the UML will run as - an otherwise " .
+	"unused uid\n";
+    print "\tis a good idea\n\n";
+    print "Optional arguments:\n";
+    print "\t-n prints its commands without executing them\n\n";
+    print "\t-v prints its commands as well as executing them\n\n";
+    print "\t-net configures a network - with a non-bridged configuration, " .
+	"IP\n";
+    print "\taddresses for both ends of the TAP device are required.  With \n";
+    print "\t-bridge, the new TAP device will be bridged with the devices\n";
+    print "\tspecified.  The host-ip address will assigned to the bridge.\n";
+    print "\t-tty-log enables logging of tty traffic and process execs\n\n";
+    print "Any further arguments will simply be appended to the UML command " .
+	"line\n";
+	
     exit 1;
 }
 
@@ -18,17 +40,19 @@ my ($tap, $host_ip, $uml_ip);
 my @net_cmds = ();
 my $tty_log = 0;
 my $verbose = 0;
+my @bridge_devices = ();
+my $dry_run = 0;
 
 while(1){
     if($uml_args[0] eq "-net"){
 	(undef, $host_ip, $uml_ip, @uml_args) = @uml_args;
 	(!defined($host_ip) || !defined($uml_ip)) and Usage();
-	@net_cmds = ( "tunctl", "ifconfig", "route" );
+	push @net_cmds, ( "tunctl", "ifconfig", "route" );
     }
     elsif($uml_args[0] eq "-bridge"){
-	(undef, $uml_ip, @uml_args) = @uml_args;
-	!defined($uml_ip) and Usage();
-	@net_cmds = ( "tunctl", "ifconfig", "route" );
+	shift @uml_args;
+	push @bridge_devices, split(",", shift @uml_args);
+	push @net_cmds, "brctl";
     }
     elsif($uml_args[0] eq "-tty-log"){
 	shift @uml_args;
@@ -38,12 +62,21 @@ while(1){
         shift @uml_args;
         $verbose = 1;
     }
+    elsif($uml_args[0] eq "-n"){
+        shift @uml_args;
+        $dry_run = 1;
+    }
     else {
 	last;
     }
 }
 
 (!defined($uml) || !defined($rootfs) || !defined($uid)) and Usage();
+
+if(@bridge_devices && !defined($uml_ip)){
+    print "-bridge requires -net\n";
+    Usage();
+}
 
 my @cmds = ("mkdir", "tar", "cp", "basename", "rm", @net_cmds );
 
@@ -76,32 +109,81 @@ my $cell = "cell$n";
 my $out;
 my @more_args = ();
 
+sub run_output_status {
+    my $cmd = shift;
+
+    $verbose || $dry_run and print "$cmd\n";
+    $dry_run and return("", 0);
+
+    my $out = `$cmd 2>&1`;
+    my $status = $?;
+    $verbose and print "$out\n";
+    return($out, $status);
+}
+
+sub run_output {
+    my $cmd = shift;
+    my ($output, $status) = run_output_status($cmd);
+
+    $status ne 0 and die "Running '$cmd' failed : output = '$out'";
+    return($output);
+}
+
+sub run_status {
+    my $cmd = shift;
+    my (undef, $status) = run_output_status($cmd);
+
+    return($status);
+}
+
 sub run {
     my $cmd = shift;
-    my $out = `$cmd 2>&1`;
+    my ($output, $status) = run_output_status($cmd);
 
-    $? ne 0 and die "Running '$cmd' failed : output = '$out'";
-    if($verbose){
-        print "$cmd\n";
-        print "$out\n";
-    }
-    return($out);
+    $status ne 0 and die "Running '$cmd' failed : output = '$output'";
 }
 
 if(defined($uml_ip)){
-    $out = run("tunctl -u $uid");
+    $out = run_output("tunctl -u $uid");
     if($out =~ /(tap\d+)/){
 	$tap = $1;
 	push @more_args, "eth0=tuntap,$tap";
 
-	if(defined($host_ip)){
-	    run("$sudo ifconfig $tap $host_ip up");
-	    run("$sudo route add -host $uml_ip dev $tap");
+	my $tap_ifconfig = "$sudo ifconfig $tap up";
+	!@bridge_devices and $tap_ifconfig .= " $host_ip";
+
+	run($tap_ifconfig);
+
+	if(@bridge_devices){
+	    my @non_existant = map { run_status("ifconfig $_") ? $_ : ()
+				     } @bridge_devices;
+	    my @bridges = map { run_status("sudo brctl showstp $_") ? () : $_ 
+				} @bridge_devices;
+
+	    @non_existant + @bridges != 1 and 
+		die "-bridge must specify one already-existing bridge or " .
+		    "non-existant device";
+
+	    my $create = @non_existant;
+	    my $bridge = shift @{ [@non_existant, @bridges] };
+
+	    if($create){
+		run("sudo brctl addbr $bridge");
+		run("sudo brctl stp $bridge off");
+	    }
+
+	    map { if($_ ne $bridge){
+		      run("sudo brctl addif $bridge $_");
+		      run("sudo ifconfig $_ 0.0.0.0 promisc up");
+		  }
+	      } (@bridge_devices, $tap);
+
+	    run("sudo ifconfig $bridge $host_ip");
 	}
 	else {
-	    run("$sudo ifconfig $tap $host_ip up");
+	    defined($host_ip) and run("$sudo route add -host $uml_ip dev $tap");
+	    run("$sudo bash -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'");
 	}
-	run("$sudo bash -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'");
     }
     else {
 	die "Couldn't find tap device name in '$out'";
@@ -170,7 +252,12 @@ my @args = ( "bash", "-c", "./jail_uml $cell $uid /$uml ubd0=/$rootfs " .
 
 $sudo ne "" and unshift @args, $sudo;
 
-system @args;
+if(!$dry_run){
+    system @args;
+}
+else {
+    print join(" ", @args) . "\n";
+}
 
 -e "$cell/proc/mm" and run("$sudo umount $cell/proc/mm");
 run("$sudo rm -rf $cell");
